156d1206e77836d01abbf6a416086851af4a37c3
Ryan Dockstader <ryanldockstader@gmail.com>
2024-05-29 23:01:29 -0600

    clean up rockets

--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -2,5 +2,14 @@ extends Area2D
 
 @export var speed = 2000
 
+@onready var visable_notifier = $visableNotifier
+
+func _ready():
+	visable_notifier.connect("screen_exited", _on_screen_exited)
+
 func _physics_process(delta):
 	global_position.x += (speed * delta)
+	
+	
+func _on_screen_exited():
+	queue_free()
